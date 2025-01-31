4f8848d50658be37774869c3234fba02c903ce7d
tomodakengo <joa.tomoda@gmail.com>
2025-02-01 07:46:21 +0900

    feat: ベースモデルを作成

---
+++ backend/app/models/base.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+from sqlalchemy import Column, Integer, DateTime
+from app.core.database import Base
+
+class BaseModel(Base):
+    __abstract__ = True
+
+    id = Column(Integer, primary_key=True, index=True)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
