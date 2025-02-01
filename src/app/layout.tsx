197a2991465e8933058e4908dde5469197279c4e
tomodakengo <joa.tomoda@gmail.com>
2025-02-01 12:13:19 +0900

    fix: パッケージをインストールしビルドエラーを解消

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { Inter } from "next/font/google";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import Layout from "@/components/layout/Layout";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+    },
+  },
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ja">
+      <body className={inter.className}>
+        <QueryClientProvider client={queryClient}>
+          <Layout>{children}</Layout>
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </body>
+    </html>
+  );
+}
