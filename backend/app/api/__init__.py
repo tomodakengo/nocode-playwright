a5e0f32fef2eb74928750978c5e52163e8a63780
tomodakengo <joa.tomoda@gmail.com>
2025-02-01 20:00:51 +0900

    remake

--- backend/app/api/__init__.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from app.api import test_suite, test_case, page
-
-api_router = APIRouter()
-
-api_router.include_router(test_suite.router)
-api_router.include_router(test_case.router)
-api_router.include_router(page.router)
--- backend/app/schemas/__init__.py
+++
@@ -1,6 +0,0 @@
-from app.schemas.test_suite import TestSuiteCreate, TestSuiteUpdate, TestSuiteResponse, TestSuiteWithTestCases
-from app.schemas.test_case import TestCaseCreate, TestCaseUpdate, TestCaseResponse
-from app.schemas.page import PageCreate, PageUpdate, PageResponse
-
-# Resolve forward references
-TestSuiteWithTestCases.model_rebuild()
